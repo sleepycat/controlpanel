0d3b9f7e5cb2ca079463a55b313bffafef2092dd
Mike Williamson <mike@korora.ca>
2025-01-14 21:01:28 -0500

    Basic async_graphql setup.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-use axum::{extract::Path, routing::get, Json, Router};
-use serde::Serialize;
+use axum::{ routing::get, response::{self, IntoResponse}, Router};
+use async_graphql::{http::GraphiQLSource, Object, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_axum::GraphQL;
+use tokio::net::TcpListener;
 
-#[derive(Serialize)]
-struct Greeting {
-    message: String,
+struct Query;
+
+#[Object]
+impl Query {
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
 }
 
-async fn greet(Path(name): Path<String>)-> Json<Greeting> {
-    let greeting = Greeting {
-        message: format!("Hello, {}!", name),
-    };
-    Json(greeting)
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/{name}", get(greet));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+let schema= Schema::new(Query, EmptyMutation, EmptySubscription);
+
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     println!("🚀 Listening on {}!", listener.local_addr().unwrap());
+
     axum::serve(listener, app.into_make_service()).await.unwrap();
 }
